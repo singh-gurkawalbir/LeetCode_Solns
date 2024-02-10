b47a109345383d493cd952a37673eb8583c65a00
Gurkawalbir Singh <57713328+singh-gurkawalbir@users.noreply.github.com>
2024-02-11 04:51:10 +0530

    Create Sol.cpp

---
+++ 647. Palindromic Substrings/Sol.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int n = s.length(), ans = 0;
+        for (int i = 0; i < n; ++i) {
+            int even = palindromeCount(s, i, i + 1);
+            int odd = palindromeCount(s, i, i);
+            ans += even + odd;
+        }
+        return ans;
+    }
+
+    int palindromeCount(const string& s, int left, int right) {
+        int count = 0;
+        while (left >= 0 && right < s.length() && s[left] == s[right]) {
+            --left;
+            ++right;
+            ++count;
+        }
+        return count;
+    }
+};
+
+// Check this link to understand:
+// https://leetcode.com/problems/palindromic-substrings/solutions/4703811/interview-approach-3-approach-brute-force-expand-middle-dp/?envType=daily-question&envId=2024-02-10
