d5e8217b694072ab950116302e03af66bfb15343
Gurkawalbir Singh <57713328+singh-gurkawalbir@users.noreply.github.com>
2024-03-12 00:03:35 +0530

    Update Sol.py

--- 791. Custom Sort String/Sol.py
+++ 791. Custom Sort String/Sol.py
@@ -15,8 +15,8 @@ class Solution(object):
 
         return sorted_s
         
-1. It initializes a dictionary `char_count` with characters from `order` as keys and initial count 0.
-2. It then iterates through the characters in string `s` and increments the count in `char_count` for each character that is in `order`.
-3. It creates an empty string `sorted_s` and iterates through the characters in `order`, appending each character multiplied by its count in `char_count` to `sorted_s`.
-4. Finally, it iterates through the characters in `s` and appends any character that is not in `order` to `sorted_s`.
-5. The method returns the final sorted string `sorted_s` based on the order specified in the `order` parameter.
+# 1. It initializes a dictionary `char_count` with characters from `order` as keys and initial count 0.
+# 2. It then iterates through the characters in string `s` and increments the count in `char_count` for each character that is in `order`.
+# 3. It creates an empty string `sorted_s` and iterates through the characters in `order`, appending each character multiplied by its count in `char_count` to `sorted_s`.
+# 4. Finally, it iterates through the characters in `s` and appends any character that is not in `order` to `sorted_s`.
+# 5. The method returns the final sorted string `sorted_s` based on the order specified in the `order` parameter.
